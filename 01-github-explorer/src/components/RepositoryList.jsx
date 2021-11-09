ee58696dcca29d64870a9a7e8c0c1221f415bd2d
Iury Sousa <iurysousa13@outlook.com>
2021-11-08 23:22:18 -0300

    chore: get git repositories

--- 01-github-explorer/src/components/RepositoryList.jsx
+++ 01-github-explorer/src/components/RepositoryList.jsx
@@ -1,4 +1,7 @@
+import { useEffect, useState } from "react";
 import { RepositoryItem } from "./RepositoryItem";
+import '../styles/repositories.scss';
+
 
 const repository = {
   name: 'unform',
@@ -7,6 +10,16 @@ const repository = {
 }
 
 export function RepositoryList() {
+
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+
+    fetch('https://api.github.com/orgs/rocketseat/repos')
+      .then(response => response.json())
+      .then(data => setRepositories(data));
+
+  }, [])
 
   return (
     <section className="repository-list">
