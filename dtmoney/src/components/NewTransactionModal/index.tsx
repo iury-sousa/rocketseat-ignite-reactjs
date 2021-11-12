025b713b97d2b1a22fe3fda1322ed7b4c2933112
Iury Sousa <iurysousa13@outlook.com>
2021-11-11 22:28:16 -0300

    feat(new transaction modal): button style

--- dtmoney/src/components/NewTransactionModal/index.tsx
+++ dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import ReactModal from "react-modal";
-import { Container } from "./styles";
+import { Container, TransactionTypeContainer } from "./styles";
 import closeImg from "../../assets/close.svg";
+import incomeImg from "../../assets/income.svg";
+import outcomeImg from "../../assets/outcome.svg";
 
 type NewTransactionModalProps = {
   isOpen: boolean;
@@ -30,8 +32,19 @@ export const NewTransactionModal = ({
         <h2>Cadastrar transação</h2>
         <input type="text" placeholder="Título" />
         <input type="number" placeholder="Valor" />
-        <input type="text" placeholder="Categoria" />
 
+        <TransactionTypeContainer>
+          <button type="button">
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </button>
+          <button type="button">
+            <img src={outcomeImg} alt="Saída" />
+            <span>Saída</span>
+          </button>
+        </TransactionTypeContainer>
+
+        <input type="text" placeholder="Categoria" />
         <button type="submit">Cadastrar</button>
       </Container>
     </ReactModal>
