cbd9b221f16d52c1ec65de6ebcdd1959a9d6bbd9
Iury Sousa <iurysousa13@outlook.com>
2021-11-12 23:33:33 -0300

    feat: create transaction hook

--- dtmoney/src/components/NewTransactionModal/index.tsx
+++ dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,11 +1,10 @@
+import React, { useState } from "react";
 import ReactModal from "react-modal";
-import { Container, TransactionTypeContainer, RadioBox } from "./styles";
 import closeImg from "../../assets/close.svg";
 import incomeImg from "../../assets/income.svg";
 import outcomeImg from "../../assets/outcome.svg";
-import React, { useContext, useState } from "react";
-import { api } from "../../services/api";
-import { TransactionsContext } from "../../TransactionsContext";
+import { useTransactions } from "../../hooks/useTransactions";
+import { Container, RadioBox, TransactionTypeContainer } from "./styles";
 
 type NewTransactionModalProps = {
   isOpen: boolean;
@@ -16,7 +15,7 @@ export const NewTransactionModal = ({
   isOpen,
   onRequestClose,
 }: NewTransactionModalProps) => {
-  const { createTransaction } = useContext(TransactionsContext);
+  const { createTransaction } = useTransactions();
 
   const [title, setTitle] = useState("");
   const [amount, setAmount] = useState(0);
