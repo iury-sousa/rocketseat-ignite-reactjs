f9f257980235ca557d1d983f7bc1e8067f6254dc
Iury Sousa <iurysousa13@outlook.com>
2021-11-14 22:52:15 -0300

    chore(ignews): add stripe

--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,8 +1,16 @@
+import { GetServerSideProps } from "next";
 import Head from "next/head";
 import { SubscribeButton } from "../components/SubscribeButton";
+import { stripe } from "../services/stripe";
 import styles from "./home.module.scss";
 
-export default function Home() {
+type HomeProps = {
+  product: {
+    priceId: string;
+    amount: string;
+  };
+};
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -17,12 +25,32 @@ export default function Home() {
           </h1>
           <p>
             Get access to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve(
+    "price_1JvtuSLyyV6g0F0snjJ9FAXE" /*, {
+    expand: ["product"],
+  }*/
+  );
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat("en-US", {
+      currency: "USD",
+      style: "currency",
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: { product },
+  };
+};
