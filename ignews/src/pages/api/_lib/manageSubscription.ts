633c38fb16f34264b857c2d539b170a61d677ef0
Iury Sousa <iurysousa13@outlook.com>
2021-11-16 23:43:50 -0300

    feat(ignews - payment): implements event in webhook

--- ignews/src/pages/api/_lib/manageSubscription.ts
+++ ignews/src/pages/api/_lib/manageSubscription.ts
@@ -2,10 +2,11 @@ import { fauna } from "../../../services/fauna";
 import { query } from "faunadb";
 import { stripe } from "../../../services/stripe";
 
-const { Get, Match, Index, Select, Create, Collection } = query;
+const { Get, Match, Index, Select, Create, Collection, Replace } = query;
 export async function saveSubscription(
   subscriptionId: string,
-  customerId: string
+  customerId: string,
+  createAction = false
 ) {
   const userRef = await fauna.query(
     Select("ref", Get(Match(Index("user_by_stripe_customer_id"), customerId)))
@@ -20,7 +21,18 @@ export async function saveSubscription(
     proce_id: subscription.items.data[0].price.id,
   };
 
-  await fauna.query(
-    Create(Collection("subscriptions"), { data: subscriptionData })
-  );
+  if (createAction) {
+    await fauna.query(
+      Create(Collection("subscriptions"), { data: subscriptionData })
+    );
+  } else {
+    await fauna.query(
+      Replace(
+        Select("ref", Get(Match(Index("subscription_by_id"), subscriptionId))),
+        {
+          data: subscriptionData,
+        }
+      )
+    );
+  }
 }
--- ignews/src/pages/api/webhooks.ts
+++ ignews/src/pages/api/webhooks.ts
@@ -20,7 +20,12 @@ export const config = {
   },
 };
 
-const relevanteEvents = new Set(["checkout.session.completed"]);
+const relevanteEvents = new Set([
+  "checkout.session.completed",
+  // "customer.subscription.created",
+  "customer.subscription.updated",
+  "customer.subscription.deleted",
+]);
 
 export default async function Webhooks(
   request: NextApiRequest,
@@ -47,13 +52,25 @@ export default async function Webhooks(
     if (relevanteEvents.has(type)) {
       try {
         switch (type) {
+          // case "customer.subscription.created":
+          case "customer.subscription.updated":
+          case "customer.subscription.deleted":
+            const subscription = event.data.object as Stripe.Subscription;
+
+            await saveSubscription(
+              subscription.id,
+              subscription.customer.toString(),
+              false
+            );
+            break;
           case "checkout.session.completed":
             const checkoutSession = event.data
               .object as Stripe.Checkout.Session;
 
             await saveSubscription(
               checkoutSession.subscription.toString(),
-              checkoutSession.customer.toString()
+              checkoutSession.customer.toString(),
+              true
             );
             break;
           default:
