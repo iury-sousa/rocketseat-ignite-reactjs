5e9893c79a51e9369cb02abf1cb748e2f53914cd
Iury Sousa <iurysousa13@outlook.com>
2021-11-18 23:00:54 -0300

    feat(ignews - subscription): adds validation active subscription

--- ignews/src/pages/api/auth/[...nextauth].ts
+++ ignews/src/pages/api/auth/[...nextauth].ts
@@ -3,6 +3,20 @@ import NextAuth from "next-auth";
 import Provides from "next-auth/providers";
 import { fauna } from "../../../services/fauna";
 
+const {
+  Create,
+  Collection,
+  If,
+  Not,
+  Exists,
+  Match,
+  Index,
+  Casefold,
+  Get,
+  Select,
+  Intersection,
+} = query;
+
 export default NextAuth({
   providers: [
     Provides.GitHub({
@@ -12,21 +26,32 @@ export default NextAuth({
     }),
   ],
   callbacks: {
+    async session(session) {
+      try {
+        const userActiveSubscription = await fauna.query(
+          Get(
+            Intersection([
+              Match(
+                Index("subscription_by_user_ref"),
+                Select(
+                  "ref",
+                  Get(
+                    Match(Index("user_by_email"), Casefold(session.user.email))
+                  )
+                )
+              ),
+              Match(Index("subscription_by_status"), "active"),
+            ])
+          )
+        );
+        return { ...session, activeSubscription: userActiveSubscription };
+      } catch {
+        return { ...session, activeSubscription: null };
+      }
+    },
     async signIn(user, account, profile) {
       try {
         const { email } = user;
-
-        const {
-          Create,
-          Collection,
-          If,
-          Not,
-          Exists,
-          Match,
-          Index,
-          Casefold,
-          Get,
-        } = query;
 
         const where = Match(Index("user_by_email"), Casefold(email));
 
