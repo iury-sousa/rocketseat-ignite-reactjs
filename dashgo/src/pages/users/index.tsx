b2a909d2b6ac5c13ef7d089ab6258568c1224cbc
Iury Sousa <iurysousa13@outlook.com>
2021-11-20 22:55:25 -0300

    feat: creates pagination component

--- dashgo/src/pages/users/index.tsx
+++ dashgo/src/pages/users/index.tsx
@@ -15,6 +15,7 @@ import {
 } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
+import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function Users() {
@@ -77,6 +78,7 @@ export default function Users() {
               </Tr>
             </Tbody>
           </Table>
+          <Pagination />
         </Box>
       </Flex>
     </Box>
