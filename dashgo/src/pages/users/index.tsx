edc9a3dc035fd3fe37b211d6844d6d6f09c8762c
Iury Sousa <iurysousa13@outlook.com>
2021-11-24 00:14:22 -0300

    feat: add fetch loading into users table

--- dashgo/src/pages/users/index.tsx
+++ dashgo/src/pages/users/index.tsx
@@ -34,7 +34,7 @@ type FetchData = {
 };
 
 export default function Users() {
-  const { data, isLoading, error } = useQuery(
+  const { data, isLoading, isFetching, error } = useQuery(
     "users",
     async () => {
       const response = await fetch("http://localhost:3000/api/users");
@@ -67,6 +67,9 @@ export default function Users() {
           <Flex mb="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">
               Usuários
+              {!isLoading && isFetching && (
+                <Spinner size="sm" color="gray.500" ml="4" />
+              )}
             </Heading>
             <Link href="/users/create" passHref>
               <Button
