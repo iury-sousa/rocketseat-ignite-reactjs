f694bb240e8e926264828417c564014428afed24
Iury Sousa <iurysousa13@outlook.com>
2021-11-25 00:23:09 -0300

    feat: implements pagination logic

--- dashgo/src/pages/users/index.tsx
+++ dashgo/src/pages/users/index.tsx
@@ -91,7 +91,11 @@ export default function Users() {
                   ))}
                 </Tbody>
               </Table>
-              <Pagination />
+              <Pagination
+                totalCountOfRegisters={200}
+                currentPage={5}
+                onPageChange={() => {}}
+              />
             </>
           )}
         </Box>
