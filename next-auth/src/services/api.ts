98c032aec0199f1e468ffcee75d1e7f748028dd4
Iury Sousa <iurysousa13@outlook.com>
2021-11-27 15:37:33 -0300

    feat: performing refresh token

--- next-auth/src/services/api.ts
+++ next-auth/src/services/api.ts
@@ -1,11 +1,11 @@
-import axios, { HeadersDefaults } from "axios";
-import { parseCookies } from "nookies";
+import axios, { AxiosError, HeadersDefaults } from "axios";
+import { parseCookies, setCookie } from "nookies";
 
 export type HeaderProperties = HeadersDefaults & {
   Authorization: string;
 };
 
-const cookies = parseCookies();
+let cookies = parseCookies();
 
 export const api = axios.create({
   baseURL: "http://localhost:3333",
@@ -13,3 +13,43 @@ export const api = axios.create({
     Authorization: `Bearer ${cookies["nextauth.token"]}`,
   },
 });
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error: AxiosError) => {
+    const { status, data } = error.response ?? {};
+    if (status === 401) {
+      if (data?.code === "token.expired") {
+        cookies = parseCookies();
+
+        const { "nextauth.refreshToken": refreshToken } = cookies;
+
+        api
+          .post("/refresh", {
+            refreshToken,
+          })
+          .then((response) => {
+            const { token, refreshToken: newRefreshToken } = response.data;
+
+            setCookie(undefined, "nextauth.token", token, {
+              maxAge: 60 * 60 * 24 * 30, // 30 Dias
+              path: "/",
+            });
+
+            setCookie(undefined, "nextauth.refreshToken", newRefreshToken, {
+              maxAge: 60 * 60 * 24 * 30, // 30 Dias
+              path: "/",
+            });
+
+            api.defaults.headers = {
+              ...api.defaults.headers,
+              Authorization: `Bearer ${token}`,
+            } as HeaderProperties;
+          });
+      } else {
+      }
+    }
+  }
+);
