8b19ad57af6bda233a43f9b35beec66432eff596
Iury Sousa <iurysousa13@outlook.com>
2021-11-27 14:43:18 -0300

    feat: save authentication token and refresh token in cookies

--- next-auth/src/contexts/AuthContext.tsx
+++ next-auth/src/contexts/AuthContext.tsx
@@ -1,6 +1,7 @@
 import { createContext, ReactNode, useContext, useState } from "react";
 import { api } from "../services/api";
 import Router from "next/router";
+import { setCookie } from "nookies";
 
 type User = {
   email: string;
@@ -42,6 +43,15 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
       });
 
       const { token, refreshToken, permissions, roles } = response.data;
+
+      setCookie(undefined, "nextauth.token", token, {
+        maxAge: 60 * 60 * 24 * 30, // 30 Dias
+        path: "/",
+      });
+      setCookie(undefined, "nextauth.refreshToken", refreshToken, {
+        maxAge: 60 * 60 * 24 * 30, // 30 Dias
+        path: "/",
+      });
 
       setUser({
         email,
