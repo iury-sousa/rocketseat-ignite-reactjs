d1a4888d6b4a49b6cfbc72d61cde9dc0915ef12d
Iury Sousa <iurysousa13@outlook.com>
2021-11-27 14:34:51 -0300

    feat: authenticating the user

--- next-auth/src/contexts/AuthContext.tsx
+++ next-auth/src/contexts/AuthContext.tsx
@@ -1,12 +1,21 @@
-import { createContext, ReactNode, useContext } from "react";
+import { createContext, ReactNode, useContext, useState } from "react";
 import { api } from "../services/api";
+import Router from "next/router";
+
+type User = {
+  email: string;
+  permissions: string[];
+  roles: string[];
+};
 
 type SignInCredentials = {
   email: string;
   password: string;
 };
+
 type AuthContextData = {
   signIn(credentials: SignInCredentials): Promise<void>;
+  user?: User;
   isAuthenticated: boolean;
 };
 
@@ -14,25 +23,40 @@ type AuthProviderProps = {
   children: ReactNode;
 };
 
+type SessionResponse = Omit<User, "email"> & {
+  token: string;
+  refreshToken: string;
+};
+
 const AuthContext = createContext({} as AuthContextData);
 
 export const AuthProvider = ({ children }: AuthProviderProps) => {
-  const isAuthenticated = false;
+  const [user, setUser] = useState<User>();
+  const isAuthenticated = !!user;
+
   const signIn = async ({ email, password }: SignInCredentials) => {
     try {
-      const response = await api.post("/sessions", {
+      const response = await api.post<SessionResponse>("/sessions", {
         email,
         password,
       });
 
-      console.log(response.data);
+      const { token, refreshToken, permissions, roles } = response.data;
+
+      setUser({
+        email,
+        permissions,
+        roles,
+      });
+
+      Router.push("/dashboard");
     } catch (error) {
       console.log({ error });
     }
   };
 
   return (
-    <AuthContext.Provider value={{ isAuthenticated, signIn }}>
+    <AuthContext.Provider value={{ isAuthenticated, user, signIn }}>
       {children}
     </AuthContext.Provider>
   );
