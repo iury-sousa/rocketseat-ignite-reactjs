d1faa66a8afdc1c57e881ed62da8a1bffbadf72f
Iury Sousa <iurysousa13@outlook.com>
2021-11-30 22:40:41 -0300

    feat: memo

--- perf-react/src/components/ProductItem.tsx
+++ perf-react/src/components/ProductItem.tsx
@@ -1,10 +1,35 @@
+import { memo } from "react";
+
 type ProductItemProps = {
   product: { id: number; price: number; title: string };
 };
-export const ProductItem = ({ product }: ProductItemProps) => {
+
+const ProductItemComponent = ({ product }: ProductItemProps) => {
   return (
     <div>
       {product.title} - <strong>{product.price}</strong>
     </div>
   );
 };
+
+export const ProductItem = memo(
+  ProductItemComponent,
+  (prevProps, nextProps) => {
+    return Object.is(prevProps.product, nextProps.product);
+  }
+);
+
+/**
+ * FLuxo de renderização do React
+ * 1. Criar uma nova versão do componente
+ * 2. Comparar com a versão anterior
+ * 3. Se houverem alterações, vai atualizar o que alterou
+ */
+
+/**
+ * Quando utilizar o memo
+ * 1. Pure Functions Component - Componentes puros que exibem sempre o mesmo dado
+ * 2. Renders too often - Quando rederizam muitas vezes
+ * 3. Re-renders with same props
+ * 4. Medium to big size
+ */
