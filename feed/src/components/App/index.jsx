7977d100186ae441f78f899dcbf4f1c7f0ac7f5a
Iury Sousa <iurysousa13@outlook.com>
2022-06-06 20:45:02 -0300

    creates a post list

--- feed/src/components/App/index.jsx
+++ feed/src/components/App/index.jsx
@@ -5,6 +5,56 @@ import { Post } from '../Post';
 import styles from './styles.module.css'
 import '../../styles/global.css';
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/iury-sousa.png',
+      name: 'Iury Sousa',
+      role: 'Web Development'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: '👉 jane.design/doctorcare' },
+      // <p>
+      //   <a href="#">#novoprojeto </a>
+      //   <a href="#">#nlw </a>
+      //   <a href="#">#rocketseat </a>
+      // </p>
+    ],
+    publishedAt: new Date('2022-06-06 08:36:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/iury-sousa.png',
+      name: 'Iury Sousa',
+      role: 'Web Development'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: '👉 jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-06-06 09:24:00')
+  },
+  {
+    id: 3,
+    author: {
+      avatarUrl: 'https://github.com/iury-sousa.png',
+      name: 'Iury Sousa',
+      role: 'Web Development'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: '👉 jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-07-06 08:36:00')
+  }
+];
+
 export function App() {
   return (
     <div>
@@ -12,9 +62,13 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
-          <Post />
-          <Post />
+          {posts.map(post => (
+            <Post
+              key={post.id}
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt} />
+          ))}
         </main>
       </div>
     </div >
