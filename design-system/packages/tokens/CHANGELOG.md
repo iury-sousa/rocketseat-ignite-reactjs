e4a983fa03aa32384fa3779762b5587f431f1eff
Iury Sousa <iurysousa13@outlook.com>
2023-06-17 20:08:39 -0300

    ci: Add yarn cache

---
+++ design-system/packages/tokens/CHANGELOG.md
@@ -0,0 +1,7 @@
+# @iury-ignite-ui/tokens
+
+## 1.1.0
+
+### Minor Changes
+
+- Add new color
--- design-system/packages/tokens/src/colors.ts
+++ design-system/packages/tokens/src/colors.ts
@@ -2,6 +2,8 @@ export const colors = {
   white: '#FFF',
   black: '#000',
 
+  red: '#F00',
+
   gray100: '#E1E1E6',
   gray200: '#A9A9B2',
   gray400: '#7C7C8A',
